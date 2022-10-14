30ac2ea822ea73712a7ea19aea195fe20e7c87b4
pelvis56 <stephane.torlet@dxc.com>
2022-10-14 19:49:19 +0200

    Commit

--- modules/compute/virtual_machine/vm_windows.tf
+++ modules/compute/virtual_machine/vm_windows.tf
@@ -4,8 +4,8 @@ resource "azurecaf_name" "windows" {
 
   name          = each.value.name
   resource_type = "azurerm_windows_virtual_machine"
-  prefixes      = var.global_settings.prefixes
-  random_length = var.global_settings.random_length
+  # prefixes      = var.global_settings.prefixes
+  # random_length = var.global_settings.random_length
   clean_input   = true
   passthrough   = var.global_settings.passthrough
   use_slug      = var.global_settings.use_slug
@@ -17,8 +17,8 @@ resource "azurecaf_name" "windows_computer_name" {
 
   name          = try(each.value.computer_name, each.value.name)
   resource_type = "azurerm_windows_virtual_machine"
-  prefixes      = var.global_settings.prefixes
-  random_length = var.global_settings.random_length
+  # prefixes      = var.global_settings.prefixes
+  # random_length = var.global_settings.random_length
   clean_input   = true
   passthrough   = var.global_settings.passthrough
   use_slug      = var.global_settings.use_slug
@@ -30,8 +30,8 @@ resource "azurecaf_name" "os_disk_windows" {
 
   name          = try(each.value.os_disk.name, null)
   resource_type = "azurerm_managed_disk"
-  prefixes      = var.global_settings.prefixes
-  random_length = var.global_settings.random_length
+  # prefixes      = var.global_settings.prefixes
+  # random_length = var.global_settings.random_length
   clean_input   = true
   passthrough   = var.global_settings.passthrough
   use_slug      = var.global_settings.use_slug
